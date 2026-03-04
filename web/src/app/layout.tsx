fda8ba3f6e66ae68926340e2b3b9015372fc4168
YAO Zexin <zyaoan@connect.ust.hk>
2026-03-05 02:07:10 +0800

    Add files via upload

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,20 +1,23 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import "./globals.css";
-import { ReactNode } from "react";
-import { Suspense } from "react";
-
-// const inter = Inter({ subsets: ["latin"] });
+import { ReactNode, Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "ECEasy",
   description:
     "ECEasy: Your computer network co-pilot. An interactive, intelligent assistant tailored for computer network education.",
+  icons: {
+    icon: "/2.svg",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/2.svg" type="image/svg+xml" />
+        <title>Your App Title</title>
+      </head>
       <body>
         <Suspense fallback={null}>{children}</Suspense>
       </body>
